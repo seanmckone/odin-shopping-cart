59bad6232afb36c505974ec87e547359be8e28cc
SDMckone <seandmckone@gmail.com>
2023-10-22 19:40:16 -0400

    Update viteconfig to use camelcase for css modules

--- vite.config.js
+++ vite.config.js
@@ -9,4 +9,9 @@ export default defineConfig({
     environment: "jsdom",
     setupFiles: "./testSetup.js",
   },
+  css: {
+    modules: {
+      localsConvention: "camelCaseOnly",
+    },
+  },
 });
