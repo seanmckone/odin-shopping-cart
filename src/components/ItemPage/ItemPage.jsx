a59c441e11adef2fd7619dd719557a7f09ea8606
SDMckone <seandmckone@gmail.com>
2023-10-23 19:42:11 -0400

    Create initial item page

---
+++ src/components/ItemPage/ItemPage.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+import styles from "./ItemPage.module.css";
+import { fetchItems } from "../../utils.js";
+import StarRating from "./StarRating/StarRating";
+
+const resource = "https://fakestoreapi.com/products/";
+
+function ItemPage() {
+  const [item, setItem] = useState(null);
+  const { id } = useParams();
+
+  // Fetch item
+  useEffect(() => {
+    fetchItems(resource + id).then((response) => {
+      setItem(response);
+    });
+  }, [id]);
+
+  if (!item) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <div className={styles.itemPage}>
+      <div className={styles.itemMain}>
+        <img className={styles.itemImage} src={item.image} />
+        <div className={styles.itemInfo}>
+          <h1 className={styles.itemTitle}>{item.title}</h1>
+          <StarRating rating={item.rating.rate} />
+          <h2 className={styles.itemPrice}>${item.price}</h2>
+          <button type="button" className={styles.addToCartButton}>
+            Add to cart
+          </button>
+        </div>
+      </div>
+      <p className={styles.itemDescription}>{item.description}</p>
+    </div>
+  );
+}
+
+export default ItemPage;
