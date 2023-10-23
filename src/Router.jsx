408f3f933ef6046b68ad7fe2258ed1eea4959021
SDMckone <seandmckone@gmail.com>
2023-10-23 17:53:23 -0400

    Update router to support item and checkout pages

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,13 +1,19 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
 import StorePage from "./components/StorePage/StorePage.jsx";
+import ItemPage from "./components/ItemPage/ItemPage.jsx";
+import CheckoutPage from "./components/CheckoutPage/CheckoutPage.jsx";
 
 function Router() {
   const router = createBrowserRouter([
     {
       path: "/",
       element: <App />,
-      children: [{ index: true, element: <StorePage /> }],
+      children: [
+        { index: true, element: <StorePage /> },
+        { path: "item/:id", element: <ItemPage /> },
+        { path: "checkout", element: <CheckoutPage /> },
+      ],
     },
   ]);
 
