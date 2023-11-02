9131e035082b9c7159f6963fd815b24fc07df6a1
SDMckone <seandmckone@gmail.com>
2023-11-02 19:40:15 -0400

    Remove cartData.js from .gitignore

---
+++ src/cartData.js
@@ -0,0 +1,30 @@
+const cartData = {
+  cartItems: [
+    {
+      id: 1,
+      title: "cool item 1",
+      price: 5.99,
+      count: 3,
+    },
+    {
+      id: 2,
+      title: "cool item 2",
+      price: 145,
+      count: 1,
+    },
+    {
+      id: 3,
+      title: "cool item 3",
+      price: 45,
+      count: 12,
+    },
+    {
+      id: 4,
+      title: "cool item 4",
+      price: 1.9,
+      count: 12,
+    },
+  ],
+};
+
+export default cartData;
